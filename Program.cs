3496a5a64cb1c6ceb24becce35979581a39a0c4c
исп-22 <-22@LAB30-04>
2024-11-18 14:34:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Дано число n. Из чисел 1, 4, 9, 16, 25, ... напечатать те, которые не превышают n. 
+//Среди чисел 1, 4, 9, 16, 25, ... найти первое число, большее n.
+
+
+Console.Write("Введите n= ");
+int n = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+while ((int)Math.Pow(number, 2) < n)
+{
+Console.Write(n+">"+$"{Math.Pow(number, 2)}\t");
+number++;
+}
+{
+    int sqrt = (int)Math.Sqrt(n) + 1;
+    Console.WriteLine("Первое число=" +sqrt * sqrt);
+}
